24b43a4368acd5dfaf7e31ff199cea38b5ee4e2c
Natanaeljps <natanaeljps@gmail.com>
2022-10-13 13:11:18 -0400

    ajuste no código

--- global/global.js
+++ global/global.js
@@ -1,4 +1,19 @@
-//Validação de E-mail
+//========Validação de E-mail
+
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
   }
+
+  //==============================================
+
+  function btLogin() {
+    window.location.href = "/page/login.html";    
+}
+
+  //==============================================
+
+  function btCadastrar() {
+    window.location.href = "/page/cadastro.html";    
+}
+
+  //==============================================
